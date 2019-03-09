95fd70fe07525d9912dffab898f4d803a5e25f33
katia1802 <katia.rojas.sandoval@gmail.com>
2019-03-09 17:34:47 +0100

    All data painted

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
-import React, { Component } from 'react';
-import {getPokemons} from './services/pokemonService'
-import './App.css';
-
+import React, { Component } from "react";
+import { getPokemons } from "./services/pokemonService";
+import "./App.css";
 
 class App extends Component {
-  constructor (props){
-    super (props);
+  constructor(props) {
+    super(props);
 
     this.state = {
       principalData: [],
       data: []
-    }
+    };
   }
 
-  componentDidMount (){
-    getPokemons ()
-    .then (data => {
-      return data.results.map (pokemonURL => {
-        fetch (pokemonURL.url)
-        .then (response => response.json ())
-        .then (pokeData => {
-          this.setState ({
-            data: this.state.data.concat (pokeData)
-          })
-        })      
-      return this.setState({
-        principalData: this.state.principalData.concat(pokemonURL)
-      })
-      })
-  })
+  componentDidMount() {
+    getPokemons().then(data => {
+      return data.results.map(pokemonURL => {
+        fetch(pokemonURL.url)
+          .then(response => response.json())
+          .then(pokeData => {
+            this.setState({
+              data: this.state.data.concat(pokeData)
+            });
+          });
+        return this.setState({
+          principalData: this.state.principalData.concat(pokemonURL)
+        });
+      });
+    });
   }
   render() {
-
-    const principalDataToPaint = this.state.principalData;
-    console.log ('principalDataToPaint', principalDataToPaint)
-
     const dataToPaint = this.state.data;
-    console.log ('dataToPaint', dataToPaint)
+    console.log (dataToPaint)
 
     return (
       <div className="app">
-      holi
+        <header>holi</header>
+
+        <main />
+
+        <ul>
+          {dataToPaint.map(item => {
+            return (
+              <div>
+                <li>
+                  <img src={item.sprites.front_default} alt={item.name}/>
+                  <p>{item.id}</p>
+                  <p>{item.name}</p>
+                  <ul>{item.types.map(type =>{
+                    return <li>{type.type.name}</li>
+                  })}
+
+                  </ul>
+                </li>
+              </div>
+            );
+          })}
+        </ul>
       </div>
     );
   }
